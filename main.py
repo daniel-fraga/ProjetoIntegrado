1d331e8d4feae606ff63c184086f138cebf76801
Ricalvim <RicalvimA@hotmail.com>
2019-05-12 15:28:01 -0300

    Add argparse to image path as param

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 import pytesseract
+import argparse
+
+
+def argparser():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, type=str,
+                    help="path to input image file")
+    return vars(ap.parse_args())
 
 
 def image_treatment(image):
@@ -15,8 +23,13 @@ def image_treatment(image):
 
 
 def main():
+    args = argparser()
+
+    cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+
     # read a image, flag 0 is a gray scale image
-    img = cv2.imread("./img4.jpeg", 0)
+    img = cv2.imread("./{}".format(args["image"]), 0)
+
     treated_image = image_treatment(img)
 
     cv2.imshow("Image", treated_image)
